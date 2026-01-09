baf748186be8dc3389aecb2e8812d669c0d63b26
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-01-09 23:58:09 +0530

    options logic update

--- pages/Trivia.jsx
+++ pages/Trivia.jsx
@@ -31,30 +31,30 @@ export default function Trivia() {
     return (
       <li className="trivia-que" key={dt.question}>
         <h4 dangerouslySetInnerHTML={{ __html: dt.question }} />
-        {mainData.type === "boolean" ? (
+        {/* {mainData.type === "boolean" ? (
           <div className="answer-wrap">
             <label htmlFor="true">
-              <input type="radio" name="answer" />
+              <input type="radio" name={`answer ${dt.question}`} />
               True
             </label>
 
             <label htmlFor="false">
-              <input type="radio" name="answer" />
+              <input type="radio" name={`answer ${dt.question}`} />
               False
             </label>
           </div>
-        ) : (
+        ) : ( */}
           <div className="answer-wrap">
             {[...dt.incorrect_answers, dt.correct_answer]
               .sort(() => Math.random() - 0.4)
               .map((ans) => (
                 <label key={ans}>
-                  <input type="radio" name="answer" />
+                  <input type="radio" name={`answer ${dt.question}`} />
                   {ans}
                 </label>
               ))}
           </div>
-        )}
+        {/* )} */}
       </li>
     );
   });
