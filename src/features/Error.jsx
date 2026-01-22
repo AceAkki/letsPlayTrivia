065230833dc8911269375adccc5041cf40c667c7
Akshay R (A-K-SHA-Y) <akkitricks@gmail.com>
2026-01-22 23:49:34 +0530

    up

--- src/features/Error.jsx
+++ src/features/Error.jsx
@@ -1,25 +1,25 @@
 import { useEffect } from "react";
-import {useNavigate, useRouteError} from "react-router-dom"
+import { useNavigate, useRouteError } from "react-router-dom";
 
-export default function Error (){
-    let navigate = useNavigate();
-    let err = useRouteError();
-    console.log(err)
+export default function Error() {
+  let navigate = useNavigate();
+  let err = useRouteError();
+  // console.log(err);
 
-    // Redirect to home after 5 seconds
-    useEffect(()=> {
-        setTimeout(()=> {
-            navigate("/")
-        }, 5000)
-    }, [])
+  // Redirect to home after 5 seconds
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  }, []);
 
-    return (
-          <section>
-         <h1>
-            Error Occured
-        </h1>
-        <p>{err.message}</p>
-        <p> You'll be redirected to the Home page now!</p>
-       </section>
-    )
+  // Display error message
+  return (
+    <section>
+      <h1>Error Occured</h1>
+
+      <p>{err.message}</p>
+      <p> You'll be redirected to the Home page now!</p>
+    </section>
+  );
 }
